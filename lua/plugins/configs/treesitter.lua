afa97c2329eb06891110d1cfd676471da42ac8b8
Matvey Bodrov <matvey_bodrov@ozon.ru>
2024-10-08 22:56:54 +0300

    init

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,12 @@
+local options = {
+  ensure_installed = { "lua", "vim", "vimdoc", "go", "proto" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
