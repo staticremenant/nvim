cd2eeb0e8a809a7ab339c3ecbda15410ed277e50
Matvey Bodrov <matvey_bodrov@ozon.ru>
2024-10-26 03:00:38 +0300

    some changes

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -10,10 +10,10 @@ M.ui = {
   hl_add = {},
   hl_override = {},
   changed_themes = {},
-  theme_toggle = { "chadracula", "onedark" },
-  theme = "chadracula",        -- default theme
+  theme_toggle = { "chadracula", "everforest" },
+  theme = "chadracula",       -- default theme
   transparency = false,
-  lsp_semantic_tokens = false, -- needs nvim v0.9, just adds highlight groups for lsp semantic tokens
+  lsp_semantic_tokens = true, -- needs nvim v0.9, just adds highlight groups for lsp semantic tokens
 
   -- https://github.com/NvChad/base46/tree/v2.0/lua/base46/extended_integrations
   extended_integrations = {}, -- these aren't compiled by default, ex: "alpha", "notify"
