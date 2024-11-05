4fdeff157b659443e018adf70e1a6feabdc42cca
Matvey Bodrov <matvey_bodrov@ozon.ru>
2024-11-05 20:38:09 +0300

    rollback to virtual text

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,84 +1,3 @@
-local last_echo = { false, -1, -1 }
-local echo_timer = nil
-local echo_timeout = 30
-local warning_hlgroup = 'WarningMsg'
-local error_hlgroup = 'ErrorMsg'
-local info_hlgroup = 'InfoMsg'
-local hint_hlgroup = 'HintMsg'
-local short_line_limit = 20
-
-local function echo_diagnostic()
-  if echo_timer then
-    echo_timer:stop()
-  end
-
-  echo_timer = vim.defer_fn(
-    function()
-      local line = vim.fn.line('.') - 1
-      local bufnr = vim.api.nvim_win_get_buf(0)
-
-      if last_echo[1] and last_echo[2] == bufnr and last_echo[3] == line then
-        return
-      end
-
-      local diags = vim.diagnostic.get(bufnr, {
-        lnum = line,
-        -- severity = {
-        --   min = vim.diagnostic.severity.WARN,
-        -- },
-      })
-
-      if #diags == 0 then
-        if last_echo[1] then
-          last_echo = { false, -1, -1 }
-
-          vim.api.nvim_command('echo ""')
-        end
-
-        return
-      end
-
-      last_echo = { true, bufnr, line }
-
-      local diag = diags[1]
-      local width = vim.api.nvim_get_option('columns') - 15
-      local lines = vim.split(diag.message, "\n")
-      local message = lines[1]
-      local trimmed = false
-
-      if #lines > 1 and #message <= short_line_limit then
-        message = message .. ' ' .. lines[2]
-      end
-
-      if width > 0 and #message >= width then
-        message = message:sub(1, width) .. '...'
-      end
-
-      local kind = 'warning'
-      local hlgroup = warning_hlgroup
-
-      if diag.severity == vim.diagnostic.severity.ERROR then
-        kind = 'error'
-        hlgroup = error_hlgroup
-      elseif diag.severity == vim.diagnostic.severity.INFO then
-        kind = 'info'
-        hlgroup = info_hlgroup
-      elseif diag.severity == vim.diagnostic.severity.HINT then
-        kind = 'hint'
-        hlgroup = hint_hlgroup
-      end
-
-      local chunks = {
-        { kind .. ': ', hlgroup },
-        { message }
-      }
-
-      vim.api.nvim_echo(chunks, false, {})
-    end,
-    echo_timeout
-  )
-end
-
 return {
   {
     "williamboman/mason.nvim",
@@ -116,14 +35,14 @@ return {
               return
             end
             config = config or {}
-            config.virtual_text = false
+            config.virtual_text = true
             vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
           end,
         },
       }
 
       vim.diagnostic.config({
-        virtual_text = false,
+        virtual_text = true,
         signs = true,
         underline = true,
         update_in_insert = false,
@@ -132,13 +51,6 @@ return {
           source = true,
           border = "rounded",
         },
-      })
-
-      vim.api.nvim_create_autocmd("CursorMoved", {
-        pattern = "*",
-        callback = function()
-          echo_diagnostic()
-        end,
       })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
