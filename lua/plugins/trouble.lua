4f89ad29fe778220688170e7e75528939121a051
Matvey Bodrov <matvey_bodrov@ozon.ru>
2024-11-07 02:23:25 +0300

    trouble focus

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -8,8 +8,8 @@ return {
         use_diagnostic_signs = true,
       })
 
-      vim.keymap.set("n", "<leader>tw", "<cmd>Trouble diagnostics toggle<CR>", { desc = "Toggle Trouble" })
-      vim.keymap.set("n", "<leader>tb", "<cmd>Trouble diagnostics toggle focus=false filter.buf=0<CR>", {})
+      vim.keymap.set("n", "<leader>tw", "<cmd>Trouble diagnostics toggle focus=true<CR>", {})
+      vim.keymap.set("n", "<leader>tb", "<cmd>Trouble diagnostics toggle focus=true filter.buf=0<CR>", {})
     end,
   },
 }
